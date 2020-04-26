72ea0cc224096278711b825adb9dd6250add9c94
Erin Power <xampprocky@gmail.com>
2020-04-26 19:39:28 +0200

    Removed dbg statement

--- src/loader/static_loader.rs
+++ src/loader/static_loader.rs
@@ -107,7 +107,7 @@ impl StaticLoader {
             if let Some(message) = bundle.get_message(text_id).and_then(|m| m.value) {
                 let mut errors = Vec::new();
 
-                let value = bundle.format_pattern(&message, dbg!(args), &mut errors);
+                let value = bundle.format_pattern(&message, args, &mut errors);
 
                 if errors.is_empty() {
                     Some(value.into())
